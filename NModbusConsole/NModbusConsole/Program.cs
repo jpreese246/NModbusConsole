c9ab183a1289a877e57800ed62c46ddaa9a40697
jpreese246 <jpreese@gmail.com>
2016-11-20 10:59:47 -0500

    Practice Commit

--- NModbusConsole/NModbusConsole/Program.cs
+++ NModbusConsole/NModbusConsole/Program.cs
@@ -38,7 +38,7 @@ namespace ChamberView
                 //ushort[] ReadHoldingRegisters(
                 byte slaveId = 1;
                 ushort startAddress = 300;
-                ushort value = 770;
+                ushort value = 670;
                 master.WriteSingleRegister(slaveId, startAddress, value);
                 //ushort[] holding_register = master.ReadHoldingRegisters(slaveId, startAddress, 1);
             }
